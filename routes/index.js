f725789dd8af69e837f5a8212e7ab6bca3dd872f
luizalinda <84139409+luizalinda@users.noreply.github.com>
2022-05-12 09:32:27 -0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ module.exports = (app)=>{
     
     //rota aleatória
     app.get('/',(req,res)=>{
-    res.send("qualquer coisa")
+    //res.send("qualquer coisa")
+    res.render('index.ejs')
     })
 } 

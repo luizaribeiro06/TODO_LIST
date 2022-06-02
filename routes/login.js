bfe960eb8bf0c774cc34360a200320acabdfd860
luizalinda <84139409+luizalinda@users.noreply.github.com>
2022-06-02 09:29:36 -0300

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -23,7 +23,7 @@ module.exports = (app)=>{
         }
         //buscar os documentos na coleção atividades desse usuário
         const atividades = require('../models/atividades')
-        var buscar = await atividades.find({usuario:req.body.id})
+        var buscar = await atividades.find({usuario:procurar._id}) //esse procurar ta acessando a collection usuarios, qnd vem da collection tem _
         //abrir a view atividades e enviar nome e id
         res.render('atividades.ejs', {nome:procurar.nome,id:procurar._id,dados:buscar})
     })

8bbec1aece12d67b1edc303d2d648aba5c25d3c0
luizalinda <84139409+luizalinda@users.noreply.github.com>
2022-06-08 08:51:32 -0300

    Add files via upload

--- routes/atividades.js
+++ routes/atividades.js
@@ -1,8 +1,22 @@
 const atividades = require('../models/atividades')
+const usuarios = require('../models/usuarios')
 
 module.exports = (app)=>{
+    //criar a rota para renderizar a view atividades
+    app.get('/atividades', async(req,res)=>{
+    //capturar o id da barra de endereço
+    var id = req.query.id
+    //buscar o nome na collection usuarios
+    var user = await usuarios.findOne({_id:id})
+    //buscar todas as atividades desse usuário 
+    var buscar = await atividades.find({usuario:id}) //esse find q gera os dados
+    //console.log(buscar)
+    res.render('atividades.ejs',{nome:user.nome,id:user._id,dados:buscar}) //n ta reconhecendo dados.nome, por isso colocou use.nome / se colocar _ no id vai buscar na collection atividades, mas o id corresponde ao documento da tividade e não do usuario, ent qnd ta mandando o id da atividade dentro não vai funcionmar, tem q ser o id do user
+    })
+
+    //gravar as informações
     app.post('/atividades', async(req,res)=>{
-        //recuperando as informações digiadas
+        //recuperando as informações digitadas
         var dados = req.body
         //exibindo no terminal 
         console.log(dados)
@@ -18,10 +32,9 @@ module.exports = (app)=>{
             disciplina:dados.disciplina,
             usuario:dados.id
         }).save()
-        //buscar todas as atividades desse ususário 
-        var buscar = await atividades.find({usuario:dados.id}) //esse find q gera os dados
-        //console.log(buscar)
-        res.render('atividades.ejs',{nome:dados.nome,id:dados.id,dados:buscar})
+       
+        //redirecionar para a rota atividades
+        res.redirect('/atividades?id='+dados.id)
     })
 
     //excluir atividades
@@ -31,33 +44,13 @@ module.exports = (app)=>{
         var excluir = await atividades.findOneAndRemove({ //pd ser findOneAndDelete tbm, faz same coisa
             _id:id
         })
-        //voltar para a página atividades
-        //res.render('atividades.ejs',{nome:dados.nome,id:dados.id,dados:buscar})
-        res.send("Atividade Excluída!!")
+        //redirecionar para  a rota atividades
+        res.redirect('/atividades?id='+excluir.usuario)
     })
 
-    //alterar atividades
-    app.get("/alterar", async(req,res)=>{
-        //recuperar o parâmetro id da barra de endereço
-        var id = req.query.id
-        var alterar = await atividades.findOneAndReplace({ //pd ser findOneAndDelete tbm, faz same coisa
-            _id:id
-        })
-        //voltar para a página atividades
-        //res.render('atividades.ejs',{nome:dados.nome,id:dados.id,dados:buscar})
-        res.send("Atividade Alterada!!")
-    })
-
-    //entregar atividades
-    app.get("/entregue", async(req,res)=>{
-        //recuperar o parâmetro id da barra de endereço
-        var id = req.query.id
-        var entregue = await atividades.findOneAndRemove({ //pd ser findOneAndDelete tbm, faz same coisa
-            _id:id
-        })
-        
-        //voltar para a página atividades
-        //res.render('atividades.ejs',{nome:dados.nome,id:dados.id,dados:buscar})
-        res.send("Atividade Entregue!!")
-    })
+    
 }
+
+//registro ok, login ok, mas gravou, erro: objeto teve o valor vazio da comparação id model usuarios
+//nos campos tipo hidden estao sem valor, só o nome tem value. logo, achamos o problem
+//qm mandou as informações p o cara atividades é o cara login (Mas ta certo pq no ele taprpcurando e levando id)
